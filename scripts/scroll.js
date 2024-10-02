cf43c4b050373d0d6c99fe75f2c63db9c28459d6
Igor B <lorax.k@yandex.ru>
2024-10-02 14:58:25 +0200

    checkout_final+additional_functionality

---
+++ scripts/scroll.js
@@ -0,0 +1,16 @@
+window.onscroll = function () {
+  var button = document.getElementById("backToTop");
+
+  if (window.innerWidth > 768) {
+    if (
+      document.body.scrollTop > 400 ||
+      document.documentElement.scrollTop > 400
+    ) {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  } else {
+    button.style.display = "none";
+  }
+};
